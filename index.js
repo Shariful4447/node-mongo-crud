fddbe74668964ba776311b408e77ae3231c1d14c
Shariful4447 <shariful.islam4447@gmail.com>
2023-04-27 17:28:33 +0200

    mongo CRUD started

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion} = require('mongodb');
 const uri = "mongodb+srv://roslinshuvo:gUui1dz3JlvzlnnG@cluster0.w2aoa6h.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -16,9 +16,22 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
+    const insertData= async () => {
+        let data=await MongoClient();
+        let result= await data.insert({
+            name:"shuvo", 
+            price:20, 
+            quantity:5
+             
+         })
+
+         console.log('inserted', result);
+    }
     await client.db("admin").command({ ping: 1 });
+    
+
+
+    
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
