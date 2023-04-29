5328a2af3cab186c78d9d296446099d780bb61ad
Shariful4447 <shariful.islam4447@gmail.com>
2023-04-29 11:21:36 +0200

    mongo CRUD from data insert done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const bodyParser = require('body-parser');
 
 
 const { MongoClient, ServerApiVersion} = require('mongodb');
@@ -13,42 +14,39 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-    try {
-         await client.connect();
-         console.log("Connected correctly to server");
-         const db = client.db('myData');
-         // Use the collection "people"
-         const col = db.collection("products");
-         // Construct a document                                                                                                                                                              
-         let product = {
-             name: 'Shuvo',
-             price:20,
-             quantity:4
-         }
-         // Insert a single document, wait for promise so we can read it back
-         const p = await col.insertOne(product);
-         // Find one document
-         const myDoc = await col.findOne();
-         // Print to the console
-         console.log(myDoc);
-        } catch (err) {
-         console.log(err.stack);
-     }
- 
-     finally {
-        await client.close();
-    }
-}
-run().catch(console.dir);
 
 
+
+//run().catch(console.dir);
+
+//middleware function
 const app=express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/index.html');
+})
+
+
+app.post('/add-product',async (req, res)=>{
+    await client.connect();
+    const db = client.db('myData');
+    // Use the collection "people"
+    const col = db.collection("products");
+
+    const product=req.body;
+    console.log(product);
+    const result = await col.insertOne(product);
+    res.redirect('/product');
+   
+
+})
+
+app.get('/product', (req, res)=>{
+    res.sendFile(__dirname + '/product.html');
 })
 
 app.listen(3000);
